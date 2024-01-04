ef6458f0478a6f46dad136d9764bf733b34b3df2
Judd Misael R. Baguio <juddmisaelbaguio@gmail.com>
2024-01-04 18:14:40 +0800

    changeS

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@ use std::net::{TcpListener, TcpStream};
 
 use anyhow::Result;
 
-fn handle_connection(stream: &mut TcpStream) {
-    let _ = stream.write_all(b"+PONG\r\n");
+fn handle_connection(stream: &mut TcpStream) -> Result<()> {
+    stream.write_all(b"+PONG\r\n")?;
+
+    Ok(())
 }
 
 fn main() -> Result<()> {
@@ -16,17 +18,17 @@ fn main() -> Result<()> {
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                handle_connection(&mut stream)
-            }
-            Err(e) => {
-                println!("error: {}", e);
+    loop {
+        for stream in listener.incoming() {
+            match stream {
+                Ok(mut stream) => {
+                    println!("accepted new connection");
+                    handle_connection(&mut stream)?
+                }
+                Err(e) => {
+                    println!("error: {}", e);
+                }
             }
         }
     }
-
-    Ok(())
 }
