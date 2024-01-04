3fcbc28c3c92ea78caf64f002f8a1557c2d74d6d
Judd Misael R. Baguio <juddmisaelbaguio@gmail.com>
2024-01-04 18:38:59 +0800

    adding concurrent connction handler

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 // Uncomment this block to pass the first stage
-use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
 use anyhow::Result;
 
-fn handle_connection(stream: &mut TcpStream) -> Result<()> {
+async fn handle_connection(stream: &mut TcpStream) -> Result<()> {
     loop {
         let mut buffer = [0; 1024];
-        match stream.read(&mut buffer) {
+        match stream.read(&mut buffer).await {
             Ok(_size) => {
                 let buf_res = b"+PONG\r\n";
 
-                stream.write_all(buf_res)?;
-                stream.flush()?;
+                stream.write_all(buf_res).await?;
+                stream.flush().await?;
             }
             Err(err) => {
                 println!("error: {}", err);
@@ -24,24 +24,17 @@ fn handle_connection(stream: &mut TcpStream) -> Result<()> {
     Ok(())
 }
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
     loop {
-        match listener.accept() {
-            Ok((mut socket, _)) => {
-                println!("accepted new connection");
-
-                handle_connection(&mut socket)?
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        let (mut socket, _) = listener.accept().await?;
+        tokio::spawn(async move { handle_connection(&mut socket).await });
     }
 }
